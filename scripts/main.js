da0793924a027d601090620428df91ac9ad46069
れのらり <56919233+renorari@users.noreply.github.com>
2023-03-17 20:21:22 +0900

    ログイン確認

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".time").forEach((element) => {
         element.innerHTML = moment(element.children.item(0).innerHTML).locale("ja").fromNow();
     });
+    if (localStorage.getItem("userinfo")) {
+        login(JSON.parse(localStorage.getItem("userinfo")).id, JSON.parse(localStorage.getItem("userinfo")).password, true);
+    }
     function resetLogin() {
         document.querySelector("#login h3").innerHTML = "ログイン";
         document.querySelector("#login_button").innerHTML = "ログイン";
         document.querySelector("#login_button").attributes.removeNamedItem("disabled");
     }
-    document.querySelector("#login").addEventListener("submit", () => {
+    function login(id, password, auto = false) {
         document.querySelector("#login_message").innerHTML = "ログイン中...";
         document.querySelector("#login_button").innerHTML = "<span class=\"material-symbols-rounded rotate\">cached</span>";
         document.querySelector("#login_button").attributes.setNamedItem(document.createAttribute("disabled"));
         fetch("https://kana.renorari.net/api/v2/account/info", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
             },
-            body: JSON.stringify({
+            "body": JSON.stringify({
                 "user": {
-                    "id": document.querySelector("#login_id").value,
-                    "password": document.querySelector("#login_password").value
+                    "id": id,
+                    "password": password
                 }
             })
         }).then((response) => {
@@ -29,12 +32,16 @@ document.addEventListener('DOMContentLoaded', () => {
                     document.querySelector("#login #login_message").innerHTML = `${user.name}さん`;
                     document.querySelector("#login_button").innerHTML = "<span class=\"material-symbols-rounded\">done</span>";
                     localStorage.setItem("userinfo", JSON.stringify(user));
-                    setTimeout(() => {
-                        document.querySelector("#login").classList.add("fadeout");
+                    if (auto) {
+                        document.querySelector("#login").style.display = "none";
+                    } else {
                         setTimeout(() => {
-                            document.querySelector("#login").style.display = "none";
-                        }, 499);
-                    }, 500);
+                            document.querySelector("#login").classList.add("fadeout");
+                            setTimeout(() => {
+                                document.querySelector("#login").style.display = "none";
+                            }, 499);
+                        }, 500);
+                    }
                 }).catch((error) => {
                     document.querySelector("#login_message").innerHTML = "エラーが発生しました<br>もう一度お試しください";
                     console.error(error);
@@ -57,5 +64,8 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error(error);
             resetLogin();
         });
+    }
+    document.querySelector("#login").addEventListener("submit", () => {
+        login(document.querySelector("#login_id").value, document.querySelector("#login_password").value);
     });
 });
